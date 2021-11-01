ed800d1fc02ff0849bd3a70ed032db0e89800be3
Trevor Anderson <tbone101@gmail.com>
2021-10-31 19:21:00 -0700

    Changes

--- users_crud/flask_app/controllers/users.py
+++ users_crud/flask_app/controllers/users.py
@@ -1,6 +1,8 @@
 from flask_app import app
 from flask import render_template, redirect, session, request, flash
 from flask_app.models.user import User
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
 
 @app.route("/")
 def index():
